38e4a51922c13cd6178cbdcf6c11f7e478337d87
Érika Ayumi Tanaka <130071179+erikaatanaka@users.noreply.github.com>
2024-05-08 08:23:59 -0300

    classes.py

--- cliente.py
+++ cliente.py
@@ -1,5 +1,5 @@
 class Cliente:
-    def __init__ (nome, anoNascimento, sexo, saldo, endereco, ativo):
+    def __init__ (self, nome, anoNascimento, sexo, saldo, endereco, ativo):
         self.nome = nome
         self.anoNascimento = anoNascimento
         self.sexo = sexo
@@ -21,10 +21,31 @@ class Cliente:
         else:
             print("O cliente", self.nome, "Inativo")
 
+
+    def calculoIdade(self):
+       anoAtual = 2024
+       idadeCliente = anoAtual - self.anoNascimento
+       print("A idade do cliente é:", idadeCliente)
+
+    def verificarSaldo(self):
+        if self.saldo > 0:
+            print("O saldo é positivo.")
+        else:
+            print("O saldo é negativo.")
+      
+
 objeto1 = Cliente("Guilherme", 1998, "M", 5000, "Rua Teste", True)
-objeto2 = Cliente("Ana", 1998, "M", -5000, "Rua Teste", True)
-objeto3 = Cliente("Evelyn", 1998, "M", 5000, "Rua Teste", False)
+objeto2 = Cliente("Ana", 2007, "M", -5000, "Rua Teste", True)
+objeto3 = Cliente("Evelyn", 2007, "M", 5000, "Rua Teste", False)
 objeto1.imprimir()
 objeto2.imprimir()
 objeto3.imprimir()
 
+objeto2.calculoIdade()
+
+objeto1.verificarSaldo()
+objeto2.verificarSaldo()
+objeto3.verificarSaldo()
+
+  
+    
