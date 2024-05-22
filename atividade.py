62d7dc28d75701dbd37f67165dd617ecfbbffb5d
Érika Ayumi Tanaka <130071179+erikaatanaka@users.noreply.github.com>
2024-05-22 08:03:18 -0300

    atividade.py

---
+++ atividade.py
@@ -0,0 +1,28 @@
+class exercicio:
+    def __init__(self):
+        print('')
+
+    def ex1(self):
+        nomeFuncio = "Érika"
+        idade = 17
+        salario = 5000
+        cargo = "gerente"
+        turno = "matutino"
+        setor = "rh"
+        print("Funcionário:" + nomeFuncio)
+        print("Idade:" + idade)
+        print("Salário:" + salario)
+        print("Cargo:" + cargo)
+        print("Turno" + turno)
+        print("Setor:" + setor)
+
+
+
+ex = exercicio()
+ex.ex1()
+
+
+    
+   
+
+
